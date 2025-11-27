c3e39037b5b2895536f67884352adb40727bb8fe
Sarah <lowpolyme@web.de>
2025-11-27 16:54:16 +0100

    assembly info file

---
+++ PathHighlightOverlay/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("PathHighlightOverlay")]
+[assembly: AssemblyDescription("Cities: Skylines mod that highlights pure pedestrian paths in the Traffic info view.")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Lowpolyme")]
+[assembly: AssemblyProduct("PathHighlightOverlay")] 
+[assembly: AssemblyCopyright("Copyright © Lowpolyme 2025")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("BF8F1D31-FB4D-4225-99E2-ECD10CE3A944")]
+
+
+[assembly: AssemblyVersion("0.1.0.0")]
+[assembly: AssemblyFileVersion("0.1.0.0")]
